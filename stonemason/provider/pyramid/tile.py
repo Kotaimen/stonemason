c2626e415d43be6340a9751bb9a15312c28161fb
Kotaimen <kotaimen.c@gmail.com>
2015-01-14 21:08:21 +0800

    TEST: Tested pyramid.tile

--- stonemason/provider/pyramid/tile.py
+++ stonemason/provider/pyramid/tile.py
@@ -13,30 +13,47 @@ __date__ = '1/8/15'
 import collections
 import time
 import mimetypes
-
+import hashlib
 import six
 
 from .serial import Hilbert
 
 
-class TileIndex(collections.namedtuple('_TileIndex', 'z x y')):
-    """ Coordinate index of a map Tile
+_TileIndex = collections.namedtuple('_TileIndex', 'z x y')
 
-    Coordinate index of a map Tile using GoogleMaps tile system.
 
-    z:
-        zoom level
-    x:
-        coordinate at longitude axis
-    y:
-        coordinate at latitude axis
+class TileIndex(_TileIndex):
+    """ Coordinate of a map Tile.
+
+    Coordinate (aka: index) of a map :class:`~stonemason.provider.pyramid.Tile`
+    using GoogleMaps style tile map system.
+
+    .. note::
+
+        `TileIndex` is a `tuple` and thus is immutable once created.
+
+
+    >>> from stonemason.provider.pyramid import TileIndex
+    >>> index = TileIndex(2, 3, 4)
+    >>> index.z
+    2
+    >>> index.x
+    3
+    >>> index.y
+    4
+
+    :param z: Zoom level.
+    :type z: int
+    :param x: Coordinate at x axis.
+    :type x: int
+    :param y: Coordinate at y axis.
+    :type y: int
     """
 
-    def __init__(self, z=0, x=0, y=0):
+    def __new__(cls, z=0, x=0, y=0):
         assert 0 <= x <= 2 ** z
         assert 0 <= y <= 2 ** z
-
-        _TileIndex.__init__(self, z, x, y)
+        return _TileIndex.__new__(cls, z, x, y)
 
     @property
     def serial(self):
@@ -45,17 +62,65 @@ class TileIndex(collections.namedtuple('_TileIndex', 'z x y')):
     def __hash__(self):
         return hash(self.serial)
 
-    def __cmp__(self, other):
-        return self.serial < other.serial
-
     def __repr__(self):
         return 'TileIndex(%d/%d/%d)' % self
 
 
-class Tile(collections.namedtuple('_Tile', 'index data mimetype mtime')):
-    """ A Tile
+_Tile = collections.namedtuple('_Tile', 'index data mimetype mtime etag')
+
+
+class Tile(_Tile):
+    """ A piece of square area in a map.
+
+    A `Tile` is a piece of square area in a rendered digital map, sliced
+    using quad-tree grid system, called :class:`~stonemason.provider.pyramid.Pyramid`.
+
+    Tile is uniquely referenced by its :class:`~stonemason.provider.pyramid.TileIndex`.
+
+    A tile object is immutable once created and has following attributes:
+
+    `index`
+
+        Tile index.
+
+    `data`
+
+        Arbitrary binary or textual data, though its usually a rendered
+        raster map image, or geo referenced features.
+
+    `mimetype`
+
+        Type of the tile data in mimetypes format, default to
+        ``application/data``.  `Tile` will never check whether `mimetype`
+        matches actual tile data format.
+
+    `mtime`
+
+        Modify time of the tile since EPOCH in seconds, if its not given in
+        the constructor, current time is used by calling :func:`time.time()`
+
+
+    `etag`
+
+        Hash of the tile data, calculated using class:`hashlib:md5` if is not
+        given in the constructor.
+
+
+    :param index: Tile index.
+    :type index: :class:`~stonemason.provider.pyramid.TileIndex`.
+    :param data: Tile data.
+    :type data: bytes
+    :param mimetype: Mimetype of the tile data.
+    :type mimetype: str
+    :param mtime: Timestamp of the tile.
+    :type mtime: float
+    :param etag: Hash of the tile data.
+    :type etag: bytes
     """
-    def __init__(self, index=None, data=None, mimetype=None, mtime=None):
+
+    def __new__(cls, index=None, data=None,
+                mimetype=None, mtime=None,
+                etag=None):
         if index is None:
             index = TileIndex()
         if data is None:
@@ -64,11 +129,19 @@ class Tile(collections.namedtuple('_Tile', 'index data mimetype mtime')):
             mimetype = 'application/data'
         if mtime is None:
             mtime = time.time()
+        if etag is None:
+            etag = hashlib.md5(data).digest()
 
         assert isinstance(index, TileIndex)
         assert isinstance(data, bytes)
-        _Tile.__init__(self, index, data, mimetype, mtime)
+        assert isinstance(mimetype, six.string_types)
+        assert isinstance(mtime, float)
+        assert isinstance(etag, six.binary_type)
 
+        return _Tile.__new__(cls, index, data, mimetype, mtime, etag)
+
+    def __hash__(self):
+        return hash(self.index.serial)
 
     def __repr__(self):
         return 'Tile(%d/%d/%d)' % self.index
---
+++ tests/stonemason/provider/pyramid/test_tile.py
@@ -0,0 +1,74 @@
+# -*- encoding: utf-8 -*-
+
+__author__ = 'kotaimen'
+__date__ = '1/14/15'
+
+import unittest
+import hashlib
+
+from stonemason.provider.pyramid import Tile, TileIndex
+
+
+class TestTileIndex(unittest.TestCase):
+    def test_init(self):
+        index1 = TileIndex()
+        self.assertEqual(index1.x, 0)
+        self.assertEqual(index1.y, 0)
+        self.assertEqual(index1.z, 0)
+        index2 = TileIndex(2, 3, 4)
+        self.assertEqual(index2.x, 3)
+        self.assertEqual(index2.y, 4)
+        self.assertEqual(index2.z, 2)
+
+    def test_repr(self):
+        index = TileIndex(2, 3, 4)
+        self.assertEqual(str(index), 'TileIndex(2/3/4)')
+
+    def test_serial(self):
+        index = TileIndex(3, 4, 5)
+        self.assertEqual(index.serial, 0xc00000000000023)
+
+    def test_eq(self):
+        index2 = TileIndex(4, 4, 5)
+        index1 = TileIndex(3, 4, 5)
+        index3 = TileIndex(3, 4, 5)
+        self.assertNotEqual(index1, index2)
+        self.assertEqual(index1, index3)
+        self.assertEqual(index1, (3, 4, 5))
+
+    def test_hash(self):
+        index = TileIndex(3, 4, 5)
+        self.assertEqual(hash(index), 0xc00000000000023)
+
+
+class TestTile(unittest.TestCase):
+    def test_init1(self):
+        tile = Tile()
+        self.assertEqual(tile.index, (0, 0, 0))
+        self.assertEqual(tile.data, b'')
+        self.assertEqual(tile.mimetype, 'application/data')
+        self.assertGreater(tile.mtime, 0)
+        self.assertEqual(tile.etag, hashlib.md5(b'').digest())
+
+    def test_init2(self):
+        tile = Tile(index=TileIndex(2, 3, 4),
+                    data=b'a tile',
+                    mimetype='text/plain',
+                    mtime=1234.)
+        self.assertEqual(tile.index, (2, 3, 4))
+        self.assertEqual(tile.data, b'a tile')
+        self.assertEqual(tile.mimetype, 'text/plain')
+        self.assertGreater(tile.mtime, 0.)
+        self.assertEqual(tile.etag, hashlib.md5(b'a tile').digest())
+
+    def test_hash(self):
+        tile = Tile(TileIndex(3, 4, 5))
+        self.assertEqual(hash(tile), 0xc00000000000023)
+
+    def test_repr(self):
+        tile = Tile(TileIndex(3, 4, 5))
+        self.assertEqual(str(tile), 'Tile(3/4/5)')
+
+
+if __name__ == '__main__':
+    unittest.main()
