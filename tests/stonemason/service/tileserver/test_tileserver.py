e44b240a4de44e9b6eb2863ce60b50a28f947ac4
ray <gliese.q@gmail.com>
2015-01-30 21:40:20 +0800

    FIX: Fix broken python 3 compatibility caused by unicode and bytes.

--- tests/stonemason/service/tileserver/test_tileserver.py
+++ tests/stonemason/service/tileserver/test_tileserver.py
@@ -7,6 +7,7 @@
 
 """
 
+import six
 import json
 import unittest
 from stonemason.service.tileserver import StoneMasonApp
@@ -23,15 +24,32 @@ class TestStoneMasonApp(unittest.TestCase):
     def test_get_theme(self):
         resp = self.client.get('/themes/brick')
         self.assertDictEqual(
-            {"result": {"name": "brick"}}, json.loads(resp.data))
+            {
+                six.u("result"): {
+                    six.u("name"): six.u("brick")
+                }
+            },
+            json.loads(resp.data.decode('utf-8'))
+        )
 
     def test_list_themes(self):
         resp = self.client.get('/themes')
-        self.assertDictEqual({"result": []}, json.loads(resp.data))
+        self.assertDictEqual(
+            {
+                six.u("result"): []
+            },
+            json.loads(resp.data.decode('utf-8'))
+        )
 
     def test_get_tile(self):
         resp = self.client.get('/tile/brick/0/0/0.png')
-        self.assertEqual("Tile(brick, 0, 0, 0, 1x, png)", resp.data)
+        self.assertEqual(
+            six.b("Tile(brick, 0, 0, 0, 1x, png)"),
+            resp.data
+        )
 
         resp = self.client.get('/tile/brick/0/0/0@2x.png')
-        self.assertEqual("Tile(brick, 0, 0, 0, 2x, png)", resp.data)
+        self.assertEqual(
+            six.b("Tile(brick, 0, 0, 0, 2x, png)"),
+            resp.data
+        )
