b60a3dcb34d3daa9fdcd72f5062cb355f862f41e
ray <gliese.q@gmail.com>
2015-02-04 00:14:53 +0800

    FEATURE: Add TileProvider

--- stonemason/provider/tileprovider/__init__.py
+++ stonemason/provider/tileprovider/__init__.py
@@ -1,6 +1,13 @@
 # -*- encoding: utf-8 -*-
+"""
+    stonemason.provider.tileprovider
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    A Tile Provider provides tile from a given cache or storage.
+
+"""
+
 
 __author__ = 'kotaimen'
 __date__ = '1/5/15'
 
-
+from .factory import TileProviderFactory, TileProvider
