f7e6d615786d4dbed0fbbda53cb1e9726e446450
ray <gliese.q@gmail.com>
2015-02-17 11:36:15 +0800

    DOCS: Update document test for theme package

--- stonemason/mason/theme/theme.py
+++ stonemason/mason/theme/theme.py
@@ -20,131 +20,25 @@ class ThemeError(Exception):
 
 
 # make theme immutable
-_Theme = namedtuple('Theme', 'name metadata pyramid cache storage')
+_MetadataConfig = namedtuple(
+    'MetadataConfig',
+    'version description attribution thumbnail center center_zoom')
 
-_MetadataConfig = namedtuple('MetadataConfig',
-                             'version description attribution thumbnail '
-                             'center center_zoom')
+_PyramidConfig = namedtuple(
+    'PyramidConfig',
+    'levels stride crs proj boundary')
 
-_PyramidConfig = namedtuple('PyramidConfig', 'levels stride crs proj boundary')
+_CacheConfig = namedtuple(
+    'CacheConfig',
+    'prototype parameters')
 
-_CacheConfig = namedtuple('CacheConfig', 'prototype parameters')
+_StorageConfig = namedtuple(
+    'StorageConfig',
+    'prototype parameters')
 
-_StorageConfig = namedtuple('StorageConfig', 'prototype parameters')
-
-
-class Theme(_Theme):
-    """Stonemason Theme
-
-    A configuration that defines how and where a
-    :class:`~stonemason.provider.tileprovider.TileProvider` retrieve its tiles.
-
-    Each `Theme` in stonemason has a name, which is also used to identify
-    the tiles to retrieve.
-
-    Normally, a theme instance also defines the following configs:
-
-        - :class:`~stonemason.mason.theme.MetadataConfig`:
-
-            Basic information about a `Provider`.
-
-        - :class:`~stonemason.mason.theme.PyramidConfig`:
-
-            Properties that defines the tile Grid of a `Provider`.
-
-        - :class:`~stonemason.mason.theme.CacheConfig`:
-
-            Properties for creating a
-            :class:`~stonemason.provider.tilecache.TileCache`.
-
-        - :class:`~stonemason.mason.theme.StorageConfig`:
-
-            Properties for creating a
-            :class:`~stonemason.provider.tilestorage.ClusterStorage`.
-
-    Samples:
-
-    >>> from stonemason.mason.theme import Theme
-    >>> t = Theme(name='sample')
-    >>> t.name
-    'sample'
-    >>> t.metadata
-    MetadataConfig(version='', description='', attribution='K&R', thumbnail=None, center=[0, 0], center_zoom=4)
-    >>> t.pyramid
-    PyramidConfig(levels=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, \
-15, 16, 17, 18, 19, 20, 21, 22], stride=1, crs='EPSG:4326', \
-proj='EPSG:3857', boundary=(-180, -85.0511, 180, 85.0511))
-    >>> t.cache
-    CacheConfig(prototype='null', parameters={})
-    >>> t.storage
-    StorageConfig(prototype='null', parameters={})
-
-
-    :type name: str
-    :param name:
-
-        A string literal that uniquely identify a theme. A valid theme name
-        should start with an alphabet character and only contain alphanumeric
-        character, the underscore and the '@' character. This is equivalent to
-        the regular expression :regexp:`^[a-zA-Z][a-zA-Z0-9@_]*$`.
-
-    :type metadata: :class:`~stonemason.mason.theme.MetadataConfig`
-    :param metadata:
-
-        Basic information about a theme, something like attribution, version.
-
-    :type pyramid: :class:`~stonemason.mason.theme.PyramidConfig`
-    :param pyramid:
-
-        Properties that defines the tile grid system. It specified properties
-        like boundary, zoom levels.
-
-    :type cache: :class:`~stonemason.mason.theme.CacheConfig`
-    :param cache:
-
-        Properties that defines the `TileCache` used to cache tiles. It
-        could be None to disable the cache.
-
-    :type storage: :class:`~stonemason.mason.theme.StorageConfig`
-    :param storage:
-
-        Properties that defines the `ClusterStorage` where tiles are stored.
-
-    """
-
-    __slots__ = ()
-
-    def __new__(cls, name=None, metadata=None,
-                pyramid=None, cache=None, storage=None):
-
-        if name is None:
-            raise ThemeError('A Theme must have a Name!')
-
-        if not re.match('^[a-zA-Z][a-zA-Z0-9@_]*$', name):
-            raise ThemeError(
-                """A valid theme name should start with an alphabet character
-                and only contain alphanumeric character, the underscore and
-                the '@' character.""")
-
-        if metadata is None:
-            metadata = MetadataConfig()
-
-        if pyramid is None:
-            pyramid = PyramidConfig()
-
-        if cache is None:
-            cache = CacheConfig()
-
-        if storage is None:
-            storage = StorageConfig()
-
-        assert isinstance(metadata, MetadataConfig)
-        assert isinstance(pyramid, PyramidConfig)
-        assert isinstance(cache, CacheConfig)
-        assert isinstance(storage, StorageConfig)
-
-        return _Theme.__new__(cls, name=name, metadata=metadata,
-                              pyramid=pyramid, cache=cache, storage=storage)
+_Theme = namedtuple(
+    'Theme',
+    'name metadata pyramid cache storage')
 
 
 class MetadataConfig(_MetadataConfig):
@@ -382,3 +276,114 @@ class StorageConfig(_StorageConfig):
         return _StorageConfig.__new__(cls, prototype=prototype,
                                       parameters=parameters)
 
+
+class Theme(_Theme):
+    """Stonemason Theme
+
+    A configuration that defines how and where a
+    :class:`~stonemason.provider.tileprovider.TileProvider` retrieve its tiles.
+
+    Each `Theme` in stonemason has a name, which is also used to identify
+    the tiles to retrieve.
+
+    Normally, a theme instance also defines the following configs:
+
+        - :class:`~stonemason.mason.theme.MetadataConfig`:
+
+            Basic information about a `Provider`.
+
+        - :class:`~stonemason.mason.theme.PyramidConfig`:
+
+            Properties that defines the tile Grid of a `Provider`.
+
+        - :class:`~stonemason.mason.theme.CacheConfig`:
+
+            Properties for creating a
+            :class:`~stonemason.provider.tilecache.TileCache`.
+
+        - :class:`~stonemason.mason.theme.StorageConfig`:
+
+            Properties for creating a
+            :class:`~stonemason.provider.tilestorage.ClusterStorage`.
+
+    Samples:
+
+    >>> from stonemason.mason.theme import Theme, PyramidConfig
+    >>> t = Theme(name='sample', pyramid=PyramidConfig(levels=range(0, 3)))
+    >>> t.name
+    'sample'
+    >>> t.metadata
+    MetadataConfig(version='', description='', attribution='K&R', thumbnail=None, center=[0, 0], center_zoom=4)
+    >>> t.pyramid
+    PyramidConfig(levels=[0, 1, 2], stride=1, crs='EPSG:4326', proj='EPSG:3857', boundary=(-180, -85.0511, 180, 85.0511))
+    >>> t.cache
+    CacheConfig(prototype='null', parameters={})
+    >>> t.storage
+    StorageConfig(prototype='null', parameters={})
+
+
+    :type name: str
+    :param name:
+
+        A string literal that uniquely identify a theme. A valid theme name
+        should start with an alphabet character and only contain alphanumeric
+        character, the underscore and the '@' character. This is equivalent to
+        the regular expression :regexp:`^[a-zA-Z][a-zA-Z0-9@_]*$`.
+
+    :type metadata: :class:`~stonemason.mason.theme.MetadataConfig`
+    :param metadata:
+
+        Basic information about a theme, something like attribution, version.
+
+    :type pyramid: :class:`~stonemason.mason.theme.PyramidConfig`
+    :param pyramid:
+
+        Properties that defines the tile grid system. It specified properties
+        like boundary, zoom levels.
+
+    :type cache: :class:`~stonemason.mason.theme.CacheConfig`
+    :param cache:
+
+        Properties that defines the `TileCache` used to cache tiles. It
+        could be None to disable the cache.
+
+    :type storage: :class:`~stonemason.mason.theme.StorageConfig`
+    :param storage:
+
+        Properties that defines the `ClusterStorage` where tiles are stored.
+
+    """
+
+    __slots__ = ()
+
+    def __new__(cls, name=None, metadata=None,
+                pyramid=None, cache=None, storage=None):
+
+        if name is None:
+            raise ThemeError('A Theme must have a Name!')
+
+        if not re.match('^[a-zA-Z][a-zA-Z0-9@_]*$', name):
+            raise ThemeError(
+                """A valid theme name should start with an alphabet character
+                and only contain alphanumeric character, the underscore and
+                the '@' character.""")
+
+        if metadata is None:
+            metadata = MetadataConfig()
+
+        if pyramid is None:
+            pyramid = PyramidConfig()
+
+        if cache is None:
+            cache = CacheConfig()
+
+        if storage is None:
+            storage = StorageConfig()
+
+        assert isinstance(metadata, MetadataConfig)
+        assert isinstance(pyramid, PyramidConfig)
+        assert isinstance(cache, CacheConfig)
+        assert isinstance(storage, StorageConfig)
+
+        return _Theme.__new__(cls, name=name, metadata=metadata,
+                              pyramid=pyramid, cache=cache, storage=storage)
