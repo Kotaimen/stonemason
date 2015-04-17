48e54dd8951c53cf6c58ed4e82846818200aff83
Kotaimen <kotaimen.c@gmail.com>
2015-04-17 15:52:20 +0800

    FEATURE: Add "base_path" parameter to mapnik cartographer

--- stonemason/renderer/cartographer/mapnik_.py
+++ stonemason/renderer/cartographer/mapnik_.py
@@ -32,12 +32,21 @@ class MapnikMapRenderer(BaseLayer, ImageMapRenderer):
                         area to render to avoid cutting symbols in target map.
     :type buffer_size: int
 
+    :param base_path: Base path where all relative paths in the xml style
+         sheet is calculated, by default, its the dirname of the style sheet.
+    :type base_path: str
+
+    :param default_scale: Default scale factor, overwrites scale factor
+        specified in the render context.
+    :type default_scale: int
+
     """
 
     def __init__(self, name,
                  style_sheet='map.xml',
-                 default_scale=None,
-                 buffer_size=0):
+                 buffer_size=0,
+                 base_path=None,
+                 default_scale=None):
         BaseLayer.__init__(self, name)
         assert isinstance(buffer_size, int)
 
@@ -47,9 +56,11 @@ class MapnikMapRenderer(BaseLayer, ImageMapRenderer):
         self._map = mapnik.Map(32, 32)
 
         filename = self._style_sheet.encode('utf-8')
-        filename = os.path.realpath(filename)
+        filename = os.path.abspath(filename)
+        if base_path is None:
+            base_path = os.path.dirname(filename)
 
-        mapnik.load_map(self._map, filename)
+        mapnik.load_map(self._map, filename, True, base_path)
         self._map.buffer_size = buffer_size
 
     def image(self, context):
