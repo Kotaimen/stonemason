a3828a720c9425d71570abb60d9d4933de479783
ray <gliese.q@gmail.com>
2015-08-17 17:58:29 +0800

    REFACTOR: move load_node back to RenderNodeFactory and rename to register_node.

--- stonemason/renderer/cartographer/image/factory.py
+++ stonemason/renderer/cartographer/image/factory.py
@@ -4,7 +4,6 @@ __author__ = 'ray'
 __date__ = '8/14/15'
 
 from stonemason.renderer.engine.factory import RenderNodeFactory
-from stonemason.renderer.engine.rendernode import RenderNode
 
 from .terminal import *
 from .transform import *
@@ -15,35 +14,31 @@ class ImageNodeFactory(RenderNodeFactory):
     def __init__(self):
         RenderNodeFactory.__init__(self)
 
-        self._load_node('image.term.color', Color)
-        self._load_node('image.term.mapnik', Mapnik_)
-        self._load_node('image.term.mapnik.composer', MapnikComposer)
-        self._load_node('image.term.relief.simple', SimpleRelief)
-        self._load_node('image.term.relief.swiss', SwissRelief)
-        self._load_node('image.term.relief.color', ColorRelief)
-        self._load_node('image.term.storage.disk', DiskStorageNode)
-        self._load_node('image.term.storage.s3', S3StorageNode)
+        self.register_node('image.term.color', Color)
+        self.register_node('image.term.mapnik', Mapnik_)
+        self.register_node('image.term.mapnik.composer', MapnikComposer)
+        self.register_node('image.term.relief.simple', SimpleRelief)
+        self.register_node('image.term.relief.swiss', SwissRelief)
+        self.register_node('image.term.relief.color', ColorRelief)
+        self.register_node('image.term.storage.disk', DiskStorageNode)
+        self.register_node('image.term.storage.s3', S3StorageNode)
 
         # load transform nodes
-        self._load_node('image.transform.filter.min', MinFilter)
+        self.register_node('image.transform.filter.min', MinFilter)
 
         # load composite nodes
-        self._load_node('image.composite.alphablender', AlphaBlender)
-        self._load_node('image.composite.imagemagick', IMComposer)
+        self.register_node('image.composite.alphablender', AlphaBlender)
+        self.register_node('image.composite.imagemagick', IMComposer)
 
         # TODO: To be deprecated in the future release
         # load legacy names
-        self._load_node('basic.color', Color)
-        self._load_node('basic.blend', AlphaBlender)
-        self._load_node('mapnik', Mapnik_)
-        self._load_node('mapnik.composer', MapnikComposer)
-        self._load_node('imagemagick', IMComposer)
-        self._load_node('relief.simple', SimpleRelief)
-        self._load_node('relief.swiss', SwissRelief)
-        self._load_node('relief.color', ColorRelief)
-        self._load_node('data.storage.disk', DiskStorageNode)
-        self._load_node('data.storage.s3', S3StorageNode)
-
-    def _load_node(self, prototype, node_class):
-        assert node_class is None or issubclass(node_class, RenderNode)
-        self._register[prototype] = node_class
+        self.register_node('basic.color', Color)
+        self.register_node('basic.blend', AlphaBlender)
+        self.register_node('mapnik', Mapnik_)
+        self.register_node('mapnik.composer', MapnikComposer)
+        self.register_node('imagemagick', IMComposer)
+        self.register_node('relief.simple', SimpleRelief)
+        self.register_node('relief.swiss', SwissRelief)
+        self.register_node('relief.color', ColorRelief)
+        self.register_node('data.storage.disk', DiskStorageNode)
+        self.register_node('data.storage.s3', S3StorageNode)
--- stonemason/renderer/engine/factory.py
+++ stonemason/renderer/engine/factory.py
@@ -3,7 +3,7 @@
 __author__ = 'ray'
 __date__ = '8/17/15'
 
-
+from .rendernode import RenderNode
 
 
 class RenderNodeFactory(object):
@@ -11,10 +11,17 @@ class RenderNodeFactory(object):
         self._register = dict()
 
     def _lookup(self, prototype):
-        init = self._register.get(prototype)
-        if init is None:
+        try:
+            init = self._register[prototype]
+        except KeyError:
             raise ValueError('Unknown prototype: "%s"!' % prototype)
+        if init is None:
+            raise ValueError('Failed to load render node: "%s"!' % prototype)
         return init
+
+    def register_node(self, prototype, node_class):
+        assert node_class is None or issubclass(node_class, RenderNode)
+        self._register[prototype] = node_class
 
     def create_terminal_node(self, name, prototype, **parameters):
         init = self._lookup(prototype)
@@ -27,4 +34,3 @@ class RenderNodeFactory(object):
     def create_composite_node(self, name, prototype, sources, **parameters):
         init = self._lookup(prototype)
         return init(name, nodes=sources, **parameters)
-
