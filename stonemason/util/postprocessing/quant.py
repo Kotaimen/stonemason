b7d6f609d0e555d19a0c15fe739393e562a21a77
Kotaimen <kotaimen.c@gmail.com>
2015-11-26 15:19:26 +0800

    FIX: Use string instead buffer since BytesIO.getbuffer() doesn't exist on py2.

--- stonemason/util/postprocessing/quant.py
+++ stonemason/util/postprocessing/quant.py
@@ -13,8 +13,8 @@ __date__ = '11/25/15'
 
 import io
 from PIL import Image
-
 import mapnik
+
 
 def imgquant(image, colors=256):
     # assert isinstance(image, Image)
@@ -23,5 +23,6 @@ def imgquant(image, colors=256):
     image.save(buf, format='PNG', compression=0, optimized=False)
 
     # See https://github.com/mapnik/mapnik/wiki/Image-IO
-    mapnik_image = mapnik.Image.frombuffer(buf.getbuffer())
+    mapnik_image = mapnik.Image.fromstring(buf.getvalue())
+
     return mapnik_image.tostring('png8:c=%d' % colors)
